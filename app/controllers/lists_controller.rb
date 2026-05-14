b1e5c579696843ebd4d9b0abc0563f6ef3670576
Alain Mimeault <lhorde@msn.com>
2026-05-14 17:12:05 +0900

    Get to mostly operational state. Add minimal styling

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,5 +5,26 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmarks = Bookmark.all()
+  end
+
+  def new
+    @list = List.new
+    # @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
