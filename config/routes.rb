607789a06424a23eadf337c0c29f738bcc6d0e30
Alain Mimeault <lhorde@msn.com>
2026-05-15 16:59:35 +0900

    Add destroy funtionality on list items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "lists#index"
   resources :lists, except: [ :edit, :update ] do
-    resources :bookmarks, only: [ :new, :create, :destroy ]
+    resources :bookmarks, only: [ :new, :create ]
   end
   resources :movies, only: [ :index ]
+  resources :bookmarks, only: [ :destroy ]
 end
